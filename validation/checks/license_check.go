c68559c9e2e8f9940d03c17e3b6a457a8bdfe33a
DRickard <drickard1967@library.ucla.edu>
2025-02-18 18:00:14 +0000

    [SERV-448] code mods inspired by ARK check

--- validation/checks/license_check.go
+++ validation/checks/license_check.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	csv "github.com/UCLALibrary/validation-service/csvutils"
 	"github.com/UCLALibrary/validation-service/validation"
+	"io"
+	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -31,9 +34,35 @@ func (check *LicenseCheck) Validate(profile string, location csv.Location, csvDa
 
 	value := csvData[location.RowIndex][location.ColIndex]
 
-	if err := csv.IsValidLicense(value); err != nil {
-		return err
+	if err := verifyLicense(value); err != nil {
+		return fmt.Errorf("License validation failed at (row: %d, column: %d) [profile: %s]: %w",
+			location.RowIndex, location.ColIndex, profile, err)
+
 	}
 
 	return nil
 }
+
+ifunc verifyLicense(license string) error {
+        r := regexp.MustCompile('^http\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\+&amp;%\$#_]*)?$')
+        if !r.MatchString(license) {
+                return fmt.Errorf("License URL %s is not in a proper format", license)
+        }
+
+        resp, err := http.Get(license)
+        if err != nil {
+                return fmt.Errorf("Error connecting to license URL: %s", err.Error())
+        }
+        defer resp.Body.Close()
+        body, err := io.ReadAll(resp.Body)
+        if err != nil {
+                return fmt.Errorf("Error reading body of license URL : %s", err.Error())
+        }
+        if len(body) == 0 {
+                return fmt.Errorf("License URL %s  appears to lack content", license)
+        }
+
+        // Supplied license is valid
+        return nil
+}
+
