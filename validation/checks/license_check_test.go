1d5234d42ef1aaff4c7ecea761de32156ba17573
DRickard <drickard1967@library.ucla.edu>
2025-02-28 18:07:20 +0000

    [SERV-448] fixes to cut-n-paste errors

--- validation/checks/license_check_test.go
+++ validation/checks/license_check_test.go
@@ -4,39 +4,44 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"go.uber.org/multierr"
 )
 
 // TestVerifyLicense checks if verifyLicense throws the correct errors when given incorrect licenses
 func TestVerifyLicense(t *testing.T) {
 	tests := []struct {
 		name        string
-		ark         string
+		license     string
 		profile     string
 		expectError bool
 		expectedErr error
 	}{
 		{
 			name:        "Valid license with Festerize profile",
-			license:     "ihttp://library.ucla.edu",
+			license:     "http://creativecommons.org/licenses/by-nc/4.0/",
 			profile:     "festerize",
 			expectError: false,
 		},
 		{
 			name:        "Invalid license (https prefix) with Festerize profile",
-			license:     "ihttps://library.ucla.edu",
+			license:     "https://library.ucla.edu",
 			profile:     "festerize",
 			expectError: true,
 		},
 		{
 			name:        "Invalid license (bad URL format) with Festerize profile",
-			license:     "ihttp://libraryDOTucla.edu",
+			license:     "http://libraryDOTedu",
 			profile:     "festerize",
 			expectError: true,
 		},
 		{
 			name:        "Invalid license (fake URL) with Festerize profile",
-			license:     "ihttp://www.example.edu",
+			license:     "http://www.example.edu",
+			profile:     "festerize",
+			expectError: true,
+		},
+		{
+			name:        "Invalid license (no body) with Festerize profile",
+			license:     "http://about:blank",
 			profile:     "festerize",
 			expectError: true,
 		},
@@ -44,7 +49,7 @@ func TestVerifyLicense(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := verifyLicense(tt.ark, tt.profile)
+			err := verifyLicense(tt.license)
 
 			if tt.expectError {
 				assert.Error(t, err)
