8395378d36948e35e7851a799a88d32cd3bb1d5e
David Rickard <drickard1967@library.ucla.edu>
2025-03-18 11:26:56 -0700

    [SERV-448] Add license validation (#100)
    * [SERV-448] initial coding
    
    * [SERV-448] code mods inspired by ARK check
    
    * [SERV-448] import tweaks
    
    * [SERV-448] fixing regexp
    
    * [SERV-448] add header check
    
    * [SERV-448] first draft of test
    
    * [SERV-448] fixes to cut-n-paste errors
    
    * [SERV-448] using error class
    
    * [SERV-448] tweaks to test, error handling
    
    * [SERV-448] more test tweaks
    
    * [SERV-448] remove blank page check
    
    * [SERV-448] rename/declare test variable
    
    * [SERV-448] removing unused var
    
    * [SERV-448] fxing regexp
    
    * [SERV-448] fix comments
    
    ---------
    
    Co-authored-by: Kevin S. Clarke <ksclarke@ksclarke.io>

---
+++ validation/checks/license_check.go
@@ -0,0 +1,84 @@
+package checks
+
+import (
+	"io"
+	"net/http"
+	"regexp"
+
+        "github.com/UCLALibrary/validation-service/validation/config"
+        "github.com/UCLALibrary/validation-service/validation/csv"
+)
+
+// Error messages
+var (
+	noProfileErr = "supplied profile cannot be nil"
+	urlFormatErr = "license URL is not in a proper format (check for HTTPS)"
+	urlConnectErr = "problem connecting to license URL"
+	urlReadErr = "problem reading body of license URL"
+)
+
+type LicenseCheck struct{
+	profiles *config.Profiles
+}
+
+func (check *LicenseCheck) NewLicenseLCheck(profiles *config.Profiles) (*LicenseCheck, error) {
+	if profiles == nil {
+		return nil, csv.NewError(noProfileErr, csv.Location{}, "nil")
+	}
+
+	return &LicenseCheck{
+		profiles: profiles,
+	}, nil
+}
+
+func (check *LicenseCheck) Validate(profile string, location csv.Location, csvData [][]string) error {
+	// license not relevant to Bucketeer processing
+	if profile == "bucketeer" {
+		return nil
+	}
+
+	if err := csv.IsValidLocation(location, csvData, profile); err != nil {
+		return err
+	}
+
+	// find the header and determine if it matches an license header
+	header, err := csv.GetHeader(location, csvData, profile)
+
+	if err != nil {
+		return err
+	}
+
+	if header != "License" {
+		return nil
+	}
+
+	value := csvData[location.RowIndex][location.ColIndex]
+
+	if err := verifyLicense(value, profile, location); err != nil {
+		return err
+
+	}
+
+	return nil
+}
+
+func verifyLicense(license string, profile string, location csv.Location) error {
+        r := regexp.MustCompile(`^^http\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$`)
+        if !r.MatchString(license) {
+                return csv.NewError(urlFormatErr, location, profile)
+        }
+
+        resp, err := http.Get(license)
+        if err != nil {
+                return csv.NewError(urlConnectErr, location, profile)
+        }
+        defer resp.Body.Close()
+        _, err = io.ReadAll(resp.Body)
+        if err != nil {
+                return csv.NewError(urlReadErr, location, profile)
+        }
+
+        // Supplied license is valid
+        return nil
+}
+
