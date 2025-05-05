1586f3f8835bdf6a2f280c545a39c8696829aad6
David Rickard <drickard1967@library.ucla.edu>
2025-05-05 07:15:33 -0700

    [SERV-1271] Check for Unicode Replacement Character (#166)
    * [SERV-1271] basic coding
    
    * [SERV-1271] unit test
    
    * [SERV-1271] registry, profile updates
    
    * [SERV-1271] typo in profiles json
    
    ---------
    
    Co-authored-by: angelahuqing <angelahuqing@gmail.com>

---
+++ validation/checks/unicode_check_test.go
@@ -0,0 +1,63 @@
+//go:build unit
+
+// This file tests UnicodeCheckValidate.
+package checks
+
+import (
+	"github.com/UCLALibrary/validation-service/validation/csv"
+	"github.com/UCLALibrary/validation-service/validation/util"
+	"github.com/stretchr/testify/assert"
+	"slices"
+	"testing"
+)
+
+// TestUnicodeCheck checks if UnicodeCheck.Validate throws the correct errors when given text with the unicode replacement char
+func TestUnicodeCheck(t *testing.T) {
+	check, err := NewUnicodeCheck(util.NewProfiles())
+	assert.NoError(t, err)
+
+	// genericLocation provides a consistent location for the purposes of test comparison.
+	var genericLocation = csv.Location{RowIndex: 0, ColIndex: 0}
+	// slices containing expected valid and invalid URLs after table tests run
+	var testInvalids = []string{ "AgglomeÌ�ration de Drummondville" }
+
+	tests := []struct {
+		name     string
+		profile  string
+		location csv.Location
+		data     [][]string
+		result   bool
+	}{
+		{
+			name:     "Valid text without replacement char",
+			profile:  "default",
+			location: genericLocation,
+			data:     [][]string{{"Lorem ipsum"},{"dolor sit amet"}},
+			result:   true,
+		},
+		{
+			name:     "Invalid text with replacement char",
+			profile:  "default",
+			location: genericLocation,
+			data:     [][]string{{"AgglomeÌ�ration de Drummondville"},{"dolor sit amet"}},
+			result:   false,
+		},
+		{
+			name:     "Invalid duplicate text",
+			profile:  "default",
+			location: genericLocation,
+			data:     [][]string{{"AgglomeÌ�ration de Drummondville"},{"dolor sit amet"}},
+			result:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := check.Validate(tt.profile, tt.location, tt.data)
+			if (err != nil && tt.result) || (err == nil && !tt.result) {
+				t.Errorf("Expected '%v' response was not found: %v", tt.name, err)
+			}
+		})
+	}
+	assert.True(t, slices.Equal(check.invalids, testInvalids))
+}
