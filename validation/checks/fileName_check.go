79ffc2b995c47dacd777d3fb31c68bbcaaf5e227
DRickard <drickard1967@library.ucla.edu>
2025-05-06 17:32:58 +0000

    ;SERV-1276] file name whitespace check

---
+++ validation/checks/fileName_check.go
@@ -0,0 +1,57 @@
+package checks
+
+import (
+	"regexp"
+
+	"github.com/UCLALibrary/validation-service/errors"
+	"github.com/UCLALibrary/validation-service/validation/csv"
+	"github.com/UCLALibrary/validation-service/validation/util"
+)
+
+// LicenseCheck validates the License field for a given profile.
+type FileNameCheck struct {
+	profiles *util.Profiles
+}
+
+// NewFileNameCheck creates a new FileNameCheck instance, which checks File Name entries for whitespace
+//
+// It returns an error if the profiles argument is nil.
+func NewFileNameCheck(profiles *util.Profiles) (*FileNameCheck, error) {
+	if profiles == nil {
+		return nil, csv.NewError(errors.NilProfileErr, csv.Location{}, "nil")
+	}
+
+	return &FileNameCheck{
+		profiles: profiles,
+	}, nil
+}
+
+// Validate checks if the File Name field in the CSV data contains whitespace
+//
+// It checks if the header is "File Name" and chwcks if the value contains whitespace
+// It returns an error if the File Name contains whitespace
+func (check *FileNameCheck) Validate(profile string, location csv.Location, csvData [][]string) error {
+	if err := csv.IsValidLocation(location, csvData, profile); err != nil {
+		return err
+	}
+
+	// find the header and determine if it matches an license header
+	header, err := csv.GetHeader(location, csvData, profile)
+
+	if err != nil {
+		return err
+	}
+
+	if header != "File Name" || location.RowIndex == 0 {
+		return nil
+	}
+
+	value := csvData[location.RowIndex][location.ColIndex]
+
+	whitespace := regexp.MustCompile(`\s`)
+	if whitespace.MatchString(value) {
+		return csv.NewError(errors.TypeWhitespaceError, location, profile)
+	}
+
+	return nil
+}
